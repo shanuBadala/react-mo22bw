93d1f915a59dd950927a997566fed66523c23067
shanuBadala <badalashanu26@gmail.com>
2020-02-24 13:27:03 +0530

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './src/reducers/index';
+import uuid from 'uuid4';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+import App from './app';
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen  nbb:)
-        </p>
-      </div>
-    );
+const initialState = {
+  questions: [
+  {
+    "id": "418123e3-240d-49c2-b9e3-6c9e61c5e70e",
+    "question": "Capital Of India?",
+    "image": "",
+    "options": {
+      "a": "Delhi",
+      "b": "Hyderabad",
+      "c": "Noida",
+    }
+  },
+  {
+    "id": "ee5e18be-0c9c-489a-82f1-72bd0388c420",
+    "question": "Capital Of US?",
+    "image": "",
+    "options": {
+      "a": "Washington",
+      "b": "Texas",
+      "c": "Delhi",
+    }
   }
+]
 }
 
-render(<App />, document.getElementById('root'));
+const middleWare = applyMiddleware(thunk);
+let store = createStore(
+  rootReducer,
+  initialState,
+  middleWare
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
